707ecb29ff3a604c240fdb321e2b4b27ab638475
adtjha <adtjha@hotmail.com>
2021-07-18 07:14:42 +0530

    tailwindcss init

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { SafeAreaView, View, Text } from "react-native";
+import tailwind from "tailwind-rn";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Press to start a retro</Text>
-      <StatusBar style="auto" />
+const App = () => (
+  <SafeAreaView style={tailwind("h-full")}>
+    <View style={tailwind("pt-12 items-center")}>
+      <View style={tailwind("bg-blue-200 px-3 py-1 rounded-full")}>
+        <Text style={tailwind("text-blue-800 font-semibold")}>
+          Hello Tailwind
+        </Text>
+      </View>
     </View>
-  );
-}
+  </SafeAreaView>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
